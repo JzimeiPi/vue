bdb5dca7d9e212c1bfdecefc3a969a98c46d99a1
JzimeiPi <252463229@qq.com>
2021-06-21 00:55:34 +0800

    vue 初步运行成功，html模版暂时是固定的

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-function console() {
-  alert('main webpack.config')
-}
+// 1.创建vue根实例（从node_modules加载vue对象）
+// 2.挂载App组件
 
-window.console = console
+import Vue from 'vue'
+import App from './App.vue'
+
+// vue接管 main.html这个模版中id为app的div的内容
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App />'
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path')
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
   mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js'
+    }
   }
 }
